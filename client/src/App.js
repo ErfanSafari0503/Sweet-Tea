fadbca9db4d9ee40e983a70849b8f52db29a23c4
ErfyElf <erfyelf@gmail.com>
2024-11-02 17:27:00 +0330

    Sign-Up component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,13 @@
+import { SignIn } from "./components/SignIn";
+
 export default function App() {
   return (
     <div>
-      <SignIn />
+      <SignUp />
     </div>
   );
 }
 
-function SignIn() {
-  return (
-    <form className="container max-w-96 h-screen m-auto	w-full px-5 py-4 text-xs flex flex-col flex-wrap">
-      <h3 className="text-start text-2xl my-6">ورود</h3>
-      <p className="mb-5">خوش آمدید! لطفا اطلاعات خود را وارد کنید.</p>
-      <div className="flex flex-col justify-around gap-2">
-        <label htmlFor="">ایمیل</label>
-        <input
-          type="email"
-          placeholder="example@gmail.com"
-          className="border-default border-stone-400 text-left p-2 rounded-lg"
-        />
-      </div>
-      <div className="flex flex-col justify-around gap-2 mt-5">
-        <label htmlFor="">پسورد</label>
-        <input
-          type="password"
-          placeholder="********"
-          className="border-default border-stone-400 text-left p-2 rounded-lg"
-        />
-        <a href="#" className="text-sky-700 active:text-sky-950">
-          <span>فراموشی رمز عبور</span>
-        </a>
-      </div>
-      <div className="flex flex-col grow justify-end gap-2">
-        <button className="w-full bg-sky-600 p-2 rounded-lg text-zinc-50">
-          ورود
-        </button>
-        <div className="flex justify-center gap-2">
-          <span>حساب کاربری ندارید؟</span>
-          <a href="#" className="text-sky-700 active:text-sky-950">
-            <span>ثبت‌ نام</span>
-          </a>
-        </div>
-      </div>
-    </form>
-  );
+function SignUp() {
+  return <form>{/* html here  */}</form>;
 }

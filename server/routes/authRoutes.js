e1e8cd94adb2ca7a43f5115c65d8ca5da220bb51
Erfan Bahramali <ErfanBahramali@gmail.com>
2024-12-23 13:17:25 +0330

    Server: Remove all for rebuild

--- server/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { signUp } = require('./../controllers/authController');
-const { validateSignUpData } = require('./../middlewares/validationMiddleware');
-
-const router = express.Router();
-
-router.post('/signup', validateSignUpData, signUp);
-
-module.exports = router;

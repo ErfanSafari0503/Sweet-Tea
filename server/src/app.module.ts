15c391f5a0ddff2b831b2ac0c7713616b753ef2c
Erfan Bahramali <ErfanBahramali@gmail.com>
2024-12-24 01:58:29 +0330

    Server: Add auth

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { UniversitiesModule } from './universities/universities.module';
+import { RolesModule } from './roles/roles.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, AuthModule, UsersModule, UniversitiesModule, RolesModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+
+BigInt.prototype["toJSON"] = function () {
+  return Number.parseInt(this.toString()) ?? this.toString();
+};
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1'
+  });
+
+  await app.listen(process.env.PORT ?? 3002);
+
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
 bootstrap();

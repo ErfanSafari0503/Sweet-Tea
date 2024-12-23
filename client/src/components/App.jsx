609ecb74e9125b5608d592e6ecd70463f292953b
ErfyElf <erfyelf@gmail.com>
2024-12-23 21:11:02 +0330

    Ruter lazy loading in app componnent.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,19 +1,24 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "../pages/Home";
-import SignUp from "../pages/SignUp";
-import SignIn from "../pages/SignIn";
-import Dashboard from "../pages/Dashboard";
+import LoadingScreen from "../components/LoadingScreen";
+
+const Home = lazy(() => import("../pages/Home"));
+const SignUp = lazy(() => import("../pages/SignUp"));
+const SignIn = lazy(() => import("../pages/SignIn"));
+const Dashboard = lazy(() => import("../pages/Dashboard"));
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route index element={<Home />} />
-          <Route path="sign-up" element={<SignUp />} />
-          <Route path="sign-in" element={<SignIn />} />
-          <Route path="dashboard" element={<Dashboard />} />
+          <Suspense fallback={<LoadingScreen />}>
+            <Route index element={<Home />} />
+            <Route path="sign-up" element={<SignUp />} />
+            <Route path="sign-in" element={<SignIn />} />
+            <Route path="dashboard" element={<Dashboard />} />
+          </Suspense>
         </Routes>
       </BrowserRouter>
     </>

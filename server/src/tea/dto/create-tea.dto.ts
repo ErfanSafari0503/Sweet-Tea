bfea3044dba9192a8626d1f1e71e0b3678fb4d5e
VaryoSc <waryor99@gmail.com>
2025-01-04 20:15:06 +0330

    Added username based tea gifting🎁

--- server/src/tea/dto/create-tea.dto.ts
+++ server/src/tea/dto/create-tea.dto.ts
@@ -1,56 +1,63 @@
 import { $Enums } from '@prisma/client';
-import { IsNotEmpty, IsOptional, IsString, IsNumber, IsEnum, IsDateString } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsNumber,
+  IsEnum,
+  IsDateString,
+} from 'class-validator';
 
 export class CreateTeaDto {
-    @IsNotEmpty()
-    @IsNotEmpty()
-    product_id: number;
+  @IsNotEmpty()
+  @IsNotEmpty()
+  product_id: number;
 
-    @IsEnum(['donate', 'gift'])
-    @IsOptional()
-    type?: $Enums.gifts_type;
-  
-    @IsNotEmpty()
-    @IsNumber()
-    receiver_user_id: number;
-  
-    @IsOptional()
-    @IsNumber()
-    sender_user_id?: number;
-  
-    @IsString()
-    @IsOptional()
-    title?: string;
-  
-    @IsString()
-    @IsOptional()
-    nickname?: string;
-  
-    @IsString()
-    @IsOptional()
-    message?: string;
-  
-    @IsNotEmpty()
-    @IsNumber()
-    total_price: number;
-  
-    @IsEnum(['active', 'received', 'cancelled'])
-    @IsOptional()
-    status?: $Enums.gifts_status;
-  
-    @IsDateString()
-    @IsOptional()
-    received_at?: Date;
-  
-    @IsDateString()
-    @IsOptional()
-    scheduled_start_at?: Date;
-  
-    @IsDateString()
-    @IsOptional()
-    scheduled_end_at?: Date;
+  @IsEnum(['donate', 'gift'])
+  @IsOptional()
+  type?: $Enums.gifts_type;
 
-    @IsNotEmpty()
-    @IsString()
-    count: number;
-  }
+  @IsOptional()
+  @IsNumber()
+  receiver_user_id?: bigint;
+
+  @IsOptional()
+  @IsNumber()
+  sender_user_id?: number;
+
+  @IsString()
+  @IsOptional()
+  title?: string;
+
+  @IsString()
+  @IsOptional()
+  nickname?: string;
+
+  @IsString()
+  @IsOptional()
+  message?: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  total_price: number;
+
+  @IsEnum(['active', 'received', 'cancelled'])
+  @IsOptional()
+  status?: $Enums.gifts_status;
+
+  @IsDateString()
+  @IsOptional()
+  received_at?: Date;
+
+  @IsDateString()
+  @IsOptional()
+  scheduled_start_at?: Date;
+
+  @IsDateString()
+  @IsOptional()
+  scheduled_end_at?: Date;
+
+  @IsNotEmpty()
+  @IsString()
+  count: number;
+}
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UsersService { }
+export class UsersService {
+    constructor(private readonly prisma: PrismaService,){}
+    async getUserByPhone(phone: string) {
+        return await this.prisma.users.findFirst({where:{ phone_number: phone}, 
+        select: {id: true}
+        })
+    }
+
+ }

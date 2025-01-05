12f716682de3e4428695fc110128d0f6c38a00bf
ErfyElf <erfyelf@gmail.com>
2025-01-06 02:44:57 +0330

    Fix errors.

--- client/src/components/Home/Navbar.jsx
+++ client/src/components/Home/Navbar.jsx
@@ -14,13 +14,13 @@ export default function Navbar({ onMenuToggled }) {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
-            class="size-10 text-white"
+            className="size-10 text-white"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
             />
           </svg>
